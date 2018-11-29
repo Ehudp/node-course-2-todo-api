57d3444f991aaea21cdb26b68bc6c3ee6788b783
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-11-29 06:52:28 +0200

    ass route by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -27,6 +28,33 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    //valid user id using isValid
+    //404 -send back empty send
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    //findById
+    //success
+    //if todo -send it back
+    //if no todo -send back 4040 with empty body
+    //error
+    //404 - and send empty back 
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo })
+    }).catch((e) => {
+        res.status(404).send();
+    });
+
 });
 
 app.listen(3000, () => {
