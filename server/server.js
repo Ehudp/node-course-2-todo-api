0642d9fffc0bda08c40facf1d5f21b2eb3c9d1b3
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-12-02 11:46:52 +0200

    add validator

--- server/server.js
+++ server/server.js
@@ -111,6 +111,20 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+
+//post /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Start listen on port ${port}`);
 });
