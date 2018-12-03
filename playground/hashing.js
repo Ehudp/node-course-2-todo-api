4dc1f3ff75b20933c77c0fd4542569b88e457bf1
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-12-03 07:17:10 +0200

    add bcrypt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,22 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc';
 
-var data = {
+bcrypt.genSalt(10, (err, salt) => {
+
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('hash', hash);
+    });
+});
+
+var hashePassword = '$2a$10$zxFt46kPXacL3oAmAIBK.u3T/oJDlIeDzZ3Oc8K70QLlBrRJxvo32';
+bcrypt.compare(password, hashePassword, (err, res) => {
+    console.log(res);
+});
+
+/* var data = {
     id: 10
 };
 
@@ -10,7 +24,7 @@ var token = jwt.sign(data, '123abc');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+console.log('decoded', decoded); */
 
 // var message = 'I am user number 3';
 
